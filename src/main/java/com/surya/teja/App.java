536de89e1f5282d2be64eea737655b5b871427f9
surya.asa@gmail.com <surya.asa@gmail.com>
2017-09-06 18:57:01 +0530

    Third Commit

--- src/main/java/com/surya/teja/App.java
+++ src/main/java/com/surya/teja/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( Another oiasi World!" );
+        System.out.println( Another NEW World!" );
     }
 }
