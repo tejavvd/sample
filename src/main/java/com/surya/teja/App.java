acc4cee6178fa45e1dd2133d7970d6333bb69d0f
surya.asa@gmail.com <surya.asa@gmail.com>
2017-09-06 19:17:25 +0530

    fifth Commit

--- src/main/java/com/surya/teja/App.java
+++ src/main/java/com/surya/teja/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Another NEW World!" );
+        System.out.println("Another NEW World! olol" );
     }
 }
